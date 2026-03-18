ffab272d52b643aeef2a735554b74942fd740d3c
Nick van Remortel <nvremort@nvremort.home>
2026-03-18 08:08:49 +0100

    Changes made in profiling.py

--- Code_L3/profiling.py
+++ Code_L3/profiling.py
@@ -5,15 +5,17 @@ from random import randint
 from cProfile import Profile
 from pstats import Stats
 from bisect import bisect_left
+from tqdm import tqdm
+
 
 def insertion_sort(data):
     result = []
-    for value in data: 
+    for value in tqdm(data, desc='Insertion sort'):
         insert_value(result, value)
     return result
 def insertion_sort2(data):
     result = []
-    for value in data:
+    for value in tqdm(data, desc='Insertion sort 2'):
         insert_value2(result, value)
     return result
 
@@ -22,27 +24,31 @@ def insert_value(array, value):
         if existing > value:
             array.insert(i, value)
             return
-        array.append(value)
+    array.append(value)
 
 # let's try a more efficient sort
 def insert_value2(array, value):
     i = bisect_left(array, value)
     array.insert(i, value)
 
-max_size = 10**5
-data = [randint(0, max_size) for _ in range(max_size)]
-test = lambda: insertion_sort(data)
-
-test2 = lambda: insertion_sort2(data)
-
+# start profiling my code
+#set up the profiler output file
+filename = 'profile.prof'  # You can change this if needed
 # Set up the profiler
 profiler = Profile()
-profiler.runcall(test)
+profiler.enable()
+max_size = 10**5
+#max_size = 10
 data = [randint(0, max_size) for _ in range(max_size)]
-profiler.runcall(test2)
+sort1 = insertion_sort(data)
+data = [randint(0, max_size) for _ in range(max_size)]
+sort2 = insertion_sort2(data)
 
-stats = Stats(profiler)
-stats.strip_dirs()
-stats.sort_stats('cumulative')
-stats.print_stats()
+
+profiler.create_stats()
+profiler.disable()
+#stats.strip_dirs()
+#stats.sort_stats('cumulative')
+#stats.print_stats()
+profiler.dump_stats(filename)
 
