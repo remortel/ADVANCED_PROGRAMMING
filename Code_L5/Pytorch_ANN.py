b7898fb0e935f67ec1f2def0249e1f67afb10240
Nick van Remortel <nick.vanremortel@uantwerpen.be>
2026-03-19 16:44:11 +0100

    added a main loop to Pytorch_ANN.py to make it capable of using more than 1 core

--- Code_L5/Pytorch_ANN.py
+++ Code_L5/Pytorch_ANN.py
@@ -234,11 +234,14 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-epochs = 1
-time0 = time()
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(trainloader, model, loss_fn, optimizer)
-    test(valloader, model, loss_fn)
-print("Done!")
-print("\nTraining Time (in minutes) =", (time() - time0) / 60)
+
+# important, put the main training loop in the main() program, if you want to use multiprocessing
+if __name__ == '__main__':
+    epochs = 50
+    time0 = time()
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(trainloader, model, loss_fn, optimizer)
+        test(valloader, model, loss_fn)
+    print("Done!")
+    print("\nTraining Time (in minutes) =", (time() - time0) / 60)
